c87c833578fe5d1ab3772a8580c9eb83d594f1a8
Laby Damaro Camara <ldamaro98@gmail.com>
2021-03-26 06:16:32 +0100

    Mise à jour des user dans mongodb

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -25,8 +25,30 @@ exports.userOne =  (req, res) => {
 exports.createUser = (req, res) => {
     newUser = req.body;
     models
-          .User(newUser)
-          .saveAsync()
+          .User( newUser )
+          .save()
           .then( (obj) => logLib.logContent(obj) )
-    //console.log(newUser);
+}
+
+exports.userUpdate = (req, res) => {
+
+    let retourJSON = obj => res.json(obj);
+
+    let option = {_id: req.body._id};
+
+    printObj = models
+                     .User
+                     .findOneAsync(req.body)
+                     .then( obj => retourJSON( obj ) );
+
+    delete req.body['_id'];
+
+    models
+          .User
+          .findOneAndUpdate(option, req.body)
+          .then( printObj => retourJSON(printObj) )
+          .then( obj => logLib.logContent(obj) )
+
+    //console.log(req.body)
+
 }
--- app/routing/users.js
+++ app/routing/users.js
@@ -1,5 +1,13 @@
 let userController = require('../controllers/userController');
+//Création de User
 app.post('/users', userController.createUser);
+
+//Mise à jour de User
+app.put('/users', userController.userUpdate);
+
+
+//Récuperation de User
 app.get('/users', userController.users);
 app.get('/users/:name', userController.userOne);
+
 
